7649bde2eca6260e71c42f5693f3fc3f74ab7033
mjawath <mjawath@gmail.com>
2015-06-04 13:03:57 +0530

    web.xml applicationContext.xml

---
+++ src/java/com/jj/WelcomeController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jj;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author user
+ */
+@Controller
+public class WelcomeController {
+   
+    @RequestMapping(value = "/welcome")
+    public String sayHello() {
+        return "welcome";
+    }
+
+    @RequestMapping(value = "/welcomeTest")
+    public String sayHelloX() {
+        return "testX";
+    }
+
+}
---
+++ web/WEB-INF/jsp/testX.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : welcome
+    Created on : Jun 4, 2015, 12:15:25 PM
+    Author     : user
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello Fucking configuration  World!</h1>
+    </body>
+</html>
